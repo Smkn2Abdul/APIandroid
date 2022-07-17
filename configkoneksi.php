a71de375d72a39e7e8e9357ce780951a04cd30e8
Smkn2Abdul <109328635+Smkn2Abdul@users.noreply.github.com>
2022-07-17 07:30:41 +0700

    Create configkoneksi.php

---
+++ configkoneksi.php
@@ -0,0 +1,18 @@
+<?php
+
+class configkoneksi
+{
+  public $servername;
+  public $username;
+  public $password;
+  public $databasename;
+  
+  public function __construct()
+  {
+   $this->servername = 'localhost';
+   $this->username = 'root';
+   $this->password = '';
+   $this->databasename = 'android';
+  }
+}
+>?
